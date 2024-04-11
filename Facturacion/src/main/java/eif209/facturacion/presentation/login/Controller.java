2ec696d528fd389e540ac168ca4be393350ecc44
unknown <isaac.mendez.rodriguez@est.una.ac.cr>
2024-04-11 16:58:33 -0600

    vista about y mejora msj error login

--- Facturacion/src/main/java/eif209/facturacion/presentation/login/Controller.java
+++ Facturacion/src/main/java/eif209/facturacion/presentation/login/Controller.java
@@ -1,4 +1,5 @@
 package eif209.facturacion.presentation.login;
+
 import eif209.facturacion.logic.Service;
 import eif209.facturacion.logic.Usuario;
 import jakarta.servlet.http.HttpSession;
@@ -6,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Optional;
 
@@ -21,19 +23,26 @@ public class Controller {
 
     @PostMapping("/presentation/login/login")
     public String login(@RequestParam("nombreUsuario") String nombreUsuario,
-                        @RequestParam("contrasena") String contrasena, HttpSession httpSession) {
+                        @RequestParam("contrasena") String contrasena, HttpSession httpSession, RedirectAttributes redirectAttributes) {
         Optional<Usuario> usuarioDBOpt = service.usuarioRead(nombreUsuario);
-        if (usuarioDBOpt.isPresent() && contrasena.equals(usuarioDBOpt.get().getContrasena())) {
+        if (usuarioDBOpt.isPresent()) {
             Usuario usuarioDB = usuarioDBOpt.get();
-
-            httpSession.setAttribute("usuario", usuarioDB);
-            if (Usuario.Rol.PROVEEDOR.equals(usuarioDB.getRol()) && Usuario.Estado.ACTIVO.equals(usuarioDB.getEstado())) {
-                return "redirect:/presentation/facturar/show";
-            } else if (Usuario.Rol.ADMINISTRADOR.equals(usuarioDB.getRol()) && Usuario.Estado.ACTIVO.equals(usuarioDB.getEstado())) {
-                return "redirect:/presentation/proveedores/show";
+            if (contrasena.equals(usuarioDB.getContrasena())) {
+                if (Usuario.Estado.ACTIVO.equals(usuarioDB.getEstado())) {
+                    httpSession.setAttribute("usuario", usuarioDB);
+                    if (Usuario.Rol.PROVEEDOR.equals(usuarioDB.getRol())) {
+                        return "redirect:/presentation/facturar/show";
+                    } else if (Usuario.Rol.ADMINISTRADOR.equals(usuarioDB.getRol())) {
+                        return "redirect:/presentation/proveedores/show";
+                    }
+                } else {
+                    redirectAttributes.addFlashAttribute("error", "Su cuenta no está activa. Por favor, contacte al administrador.");
+                    return "redirect:/presentation/login/show";
+                }
             }
         }
-        return "redirect:/presentation/login/show?error=true";
+        redirectAttributes.addFlashAttribute("error", "Nombre de usuario o contraseña incorrectos.");
+        return "redirect:/presentation/login/show";
     }
 
     @GetMapping("/presentation/login/logout")
